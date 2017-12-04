2f08e4ee188056b53b5cb41c72ebfe2d590f1a3b
Krishna Xavier <tsixav@gmail.com>
2017-12-04 21:07:14 -0200

    importação elementos da tela

--- app/src/main/java/com/artanimes/gerenciador_contatos/MainActivity.java
+++ app/src/main/java/com/artanimes/gerenciador_contatos/MainActivity.java
@@ -2,12 +2,32 @@ package com.artanimes.gerenciador_contatos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editCodigo,editNome, editTelefone, editEmail;
+    Button btnLimpar, btnSalvar, btnExcluir;
+    ListView listViewClientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editCodigo = (EditText)findViewById(R.id.editCodigo);
+        editNome = (EditText)findViewById(R.id.editNome);
+        editTelefone = (EditText)findViewById(R.id.editTelefone);
+        editEmail = (EditText)findViewById(R.id.editEmail);
+
+        btnSalvar = (Button)findViewById(R.id.btnSalvar);
+        btnExcluir = (Button)findViewById(R.id.btnExcluir);
+        btnLimpar = (Button)findViewById(R.id.btnLimpar);
+
+        listViewClientes = (ListView) findViewById(R.id.listViewClientes);
+
+
     }
 }
